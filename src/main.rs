35b8d983a3ed233028725c6a42a47c31416c8b3b
Maciek <maciek.cholewa.vip@gmail.com>
2023-07-10 22:38:50 +0200

    12.3 Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[0].clone();
+        let file_path = args[1].clone();
+
+        Config { query, file_path }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let query = &args[0];
-    let file_path = &args[1];
+    let config = Config::new(&args);
 
-    let content = fs::read_to_string(file_path)
-                    .expect("Should have been able to open the file");
+    let content =
+        fs::read_to_string(config.file_path).expect("Should have been able to open the file");
 
     println!("Content {}", content);
 }
