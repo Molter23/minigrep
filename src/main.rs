9c1aafc8875591b5edfe7765c7bc9bc8ffae8ce4
Maciek <maciek.cholewa.vip@gmail.com>
2023-07-11 17:03:36 +0200

    12.3 Add error handaling for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,35 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
     file_path: String,
 }
 
-enum Error {
-    NoArgs,
-    QueryError,
-    FilePathError,
-    ToMuchArgs
-}
-
 impl Config {
-    fn new(args: &[String]) -> Result<Config, Error> {
-        match args.len() {
-            1 => Err(Error::NoArgs),
-            2 => Err(Error::QueryError),
-            3 => {
-                let query = args[1].clone();
-                let file_path = args[2].clone();
-        
-                Ok(Config { query, file_path })
-            }
-            _ => Err(Error::ToMuchArgs),
-        
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough args");
         }
-       
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
     }
 }
-
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if let Ok(config) = Config::new(&args) {
-        let content =
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    let content =
         fs::read_to_string(config.file_path).expect("Should have been able to open the file");
 
     println!("Content {}", content);
-    } else {
-        panic!("error");
-    }
-
-
 }
