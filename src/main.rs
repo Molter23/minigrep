8da4b6637cf9869925612b706ce86c766fc2fd51
Maciek <maciek.cholewa.vip@gmail.com>
2023-07-11 16:53:46 +0200

    12.3 My error handling

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,44 @@ struct Config {
     file_path: String,
 }
 
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[0].clone();
-        let file_path = args[1].clone();
+enum Error {
+    NoArgs,
+    QueryError,
+    FilePathError,
+    ToMuchArgs
+}
 
-        Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Result<Config, Error> {
+        match args.len() {
+            1 => Err(Error::NoArgs),
+            2 => Err(Error::QueryError),
+            3 => {
+                let query = args[1].clone();
+                let file_path = args[2].clone();
+        
+                Ok(Config { query, file_path })
+            }
+            _ => Err(Error::ToMuchArgs),
+        
+        }
+       
     }
 }
 
+
+
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-
-    let content =
+    if let Ok(config) = Config::new(&args) {
+        let content =
         fs::read_to_string(config.file_path).expect("Should have been able to open the file");
 
     println!("Content {}", content);
+    } else {
+        panic!("error");
+    }
+
+
 }
